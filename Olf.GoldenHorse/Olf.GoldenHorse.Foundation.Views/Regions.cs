2c15f8efc3ff6783bc7d80b5d73782ffa0283ecc
mregan59 <mregan59@gmail.com>
2014-05-28 21:17:44 -0500

    browse button for reals

--- Olf.GoldenHorse/Olf.GoldenHorse.Foundation.Views/Regions.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Foundation.Views/Regions.cs
@@ -3,5 +3,7 @@
     public static class Regions
     {
         public const string MainShellViewRegion = "MainShellViewRegion";
+        public const string ProjectExplorerViewRegion = "ProjectExplorerViewRegion";
+        public const string WorkspaceViewRegion = "WorkspaceViewRegion";
     }
 }

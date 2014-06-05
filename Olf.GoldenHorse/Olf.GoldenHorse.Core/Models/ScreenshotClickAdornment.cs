51375dc5f391c46601a68fc43cc68435eb484d48
philiptrevino <philip.trevino@olf.com>
2014-06-05 15:23:44 -0500

    click draw correction

--- Olf.GoldenHorse/Olf.GoldenHorse.Core/Models/ScreenshotClickAdornment.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Core/Models/ScreenshotClickAdornment.cs
@@ -15,7 +15,7 @@ namespace Olf.GoldenHorse.Core.Models
             {
                 int yOffset = Resources.click.Height/2;
                 int xOffset = Resources.click.Width/2;
-                graphics.DrawImage(Resources.click, ClickX+xOffset, ClickY+yOffset);
+                graphics.DrawImage(Resources.click, ClickX-xOffset, ClickY-yOffset);
             }
         }
     }

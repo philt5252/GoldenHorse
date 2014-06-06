be8c99e334209d994d689489aa42d8c5209af0a4
philt5252 <philip.trevino@olf.com>
2014-06-06 13:01:06 -0500

    log detail icons

--- Olf.GoldenHorse/Olf.GoldenHorse.Core.Views/Converters/CategoryToImageConverter.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Core.Views/Converters/CategoryToImageConverter.cs
@@ -10,53 +10,53 @@ namespace Olf.GoldenHorse.Core.Views.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == "Error")
+            if (value.ToString().Equals("Error"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/StatusAnnotations_Critical_32xLG_color.png"));
+                return new BitmapImage(new Uri("../../WhiteImages/StatusAnnotations_Critical_32xLG_color.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "Warning")
+            else if (value.ToString().Equals("Warning"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/StatusAnnotations_Warning_32xLG_color.png"));
+                return new BitmapImage(new Uri("../../WhiteImages/StatusAnnotations_Warning_32xLG_color.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "Event")
+            else if (value.ToString().Equals("Event"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/Event_594_exp.png"));
+                return new BitmapImage(new Uri("../../WhiteImages/Event_594_exp.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "Message")
+            else if (value.ToString().Equals("Message"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/SendInstantMessage_32x32.png"));
+                return new BitmapImage(new Uri("../../WhiteImages/SendInstantMessage_32x32.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "Checkpoint")
+            else if (value.ToString().Equals("Checkpoint"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/Symbols_Complete_and_ok_White.png"));
+                return new BitmapImage(new Uri("../../WhiteImages/Symbols_Complete_and_ok_White.png", UriKind.RelativeOrAbsolute));
             }
-            if (value == "Log")
+            else if (value.Equals("Log"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/log.png"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/log.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "ProjectLogs")
+            else if (value.Equals("ProjectLogs"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/Logs.png"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/Logs.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "Project")
+            else if (value.Equals("Project"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/project.png"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/project.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "ProjectSuiteLogs")
+            else if (value.Equals("ProjectSuiteLogs"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/projectSuiteLogs.png"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/projectSuiteLogs.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "ProjectSuiteProjects")
+            else if (value.Equals("ProjectSuiteProjects"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/projectSuiteProjectspng"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/projectSuiteProjects.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "Test")
+            else if (value.Equals("Test"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/test2.png"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/test2.png", UriKind.RelativeOrAbsolute));
             }
-            else if (value == "TestGroup")
+            else if (value.Equals("TestGroup"))
             {
-                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/tests.png"));
+                return new BitmapImage(new Uri("../WhiteImages/ProjectExplorerImages/tests.png", UriKind.RelativeOrAbsolute));
             }
             return null;
         }

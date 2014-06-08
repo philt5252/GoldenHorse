3f26bacf8976bd64078a7af19a03b8dda2714642
philt5252 <philt5252@gmail.com>
2014-06-08 00:22:20 -0500

    bitmap error fix

--- Olf.GoldenHorse/Olf.GoldenHorse.Core/ViewModels/TestOperationEditorViewModel.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Core/ViewModels/TestOperationEditorViewModel.cs
@@ -1,14 +1,17 @@
 
+using Olf.GoldenHorse.Foundation.Models;
 using Olf.GoldenHorse.Foundation.ViewModels;
 
 namespace Olf.GoldenHorse.Core.ViewModels
 {
     public class TestOperationEditorViewModel : ITestOperationEditorViewModel
     {
+        private readonly TestItem testItem;
         public string[] Operations { get; protected set; }
 
-        public TestOperationEditorViewModel()
+        public TestOperationEditorViewModel(TestItem testItem)
         {
+            this.testItem = testItem;
             Operations = new[] {"one", "two", "three"};
         }
     }

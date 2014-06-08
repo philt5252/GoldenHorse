3a7ef6f41fc508da097c78fd30f4b7e37a313975
philt5252 <philip.trevino@olf.com>
2014-06-08 16:10:53 -0500

    variable manager view

---
+++ Olf.GoldenHorse/Olf.GoldenHorse.Core.Views/Views/Variables/VariableManager View.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Olf.GoldenHorse.Core.Views.Views.Variables
+{
+    /// <summary>
+    /// Interaction logic for VariableManager_View.xaml
+    /// </summary>
+    public partial class VariableManager_View : UserControl
+    {
+        public VariableManager_View()
+        {
+            InitializeComponent();
+        }
+    }
+}

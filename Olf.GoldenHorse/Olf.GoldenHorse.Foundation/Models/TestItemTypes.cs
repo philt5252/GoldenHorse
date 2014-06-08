bd7c5f2177d9c008440b873c00c16542340ce2ba
philt5252 <philt5252@gmail.com>
2014-06-08 11:21:42 -0500

    window group and process group

--- Olf.GoldenHorse/Olf.GoldenHorse.Foundation/Models/TestItemTypes.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Foundation/Models/TestItemTypes.cs
@@ -4,5 +4,7 @@
     {
         public const string OnScreenAction = "OnScreenAction";
         public const string ValidateTextAtPoint = "ValidateTextAtPoint";
+        public const string WindowGroup = "WindowGroup";
+        public const string ProcessGroup = "ProcessGroup";
     }
 }

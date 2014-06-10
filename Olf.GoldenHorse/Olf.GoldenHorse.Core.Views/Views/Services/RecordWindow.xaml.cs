63347dc79ba8c1afb6652378b6496757794b98c8
philt5252 <philip.trevino@olf.com>
2014-06-09 19:24:06 -0500

    window to front and dock panels fixed

--- Olf.GoldenHorse/Olf.GoldenHorse.Core.Views/Views/Services/RecordWindow.xaml.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Core.Views/Views/Services/RecordWindow.xaml.cs
@@ -34,5 +34,20 @@ namespace Olf.GoldenHorse.Core.Views
             ICommand validationCommand = validationListItemViewModel.CreateValidationCommand;
             validationCommand.Execute(null);
         }
+
+        private void WindowBase_Activated_1(object sender, EventArgs e)
+        {
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            this.Topmost = true;
+            this.Top = 0;
+            this.Left = 0;
+        }
+
+        private void WindowBase_Deactivated_1(object sender, EventArgs e)
+        {
+            this.Topmost = true;
+            this.Activate();
+        }
     }
 }
---
+++ Olf.GoldenHorse/Olf.GoldenHorse.Core.Views/Views/Variables/VariableTableWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace Olf.GoldenHorse.Core.Views.Views.Variables
+{
+    /// <summary>
+    /// Interaction logic for VariableTableWindow.xaml
+    /// </summary>
+    public partial class VariableTableWindow : WindowBase
+    {
+        public VariableTableWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColumnsUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            IntegerUpDown integerUpDown = sender as IntegerUpDown;
+
+            dataGrid.Columns.Add(new DataGridTemplateColumn()); 
+        }
+    }
+}

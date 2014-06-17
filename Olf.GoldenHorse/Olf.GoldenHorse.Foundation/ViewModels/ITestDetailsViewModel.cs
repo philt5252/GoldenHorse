a41bfbb82449a3484ad794572f8f93ebe80b0245
philiptrevino <philip.trevino@olf.com>
2014-06-17 09:42:07 -0500

    interface haha

--- Olf.GoldenHorse/Olf.GoldenHorse.Foundation/ViewModels/ITestDetailsViewModel.cs
+++ Olf.GoldenHorse/Olf.GoldenHorse.Foundation/ViewModels/ITestDetailsViewModel.cs
@@ -10,6 +10,9 @@ namespace Olf.GoldenHorse.Foundation.ViewModels
         ObservableCollection<ITestItemViewModel> TestItems { get; }
         ICommand PlayCommand { get; }
         ICommand DeleteSelectedItemCommand { get; }
+        ICommand AppendToEndOfTestCommand { get; }
+        ICommand AppendToStartOfTestCommand { get; }
+        ICommand AppendAfterSelectedItemCommand { get; }
         void Refresh();
     }
 }
